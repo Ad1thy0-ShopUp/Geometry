34485aa40e783a1a840df5175957f1c74414b460
Ad1thy0-ShopUp <adithyasai.l@shopup.org>
2022-08-08 10:02:54 +0530

    Create Line

--- lib/geometry.rb
+++ lib/geometry.rb
@@ -1,8 +1,17 @@
-# frozen_string_literal: true
-
-require_relative "geometry/version"
+require_relative "geometry/point"
 
 module Geometry
-  class Error < StandardError; end
-  # Your code goes here...
+  class Line
+    attr_accessor :length
+
+    def initialize(point_1, point_2)
+      if point_1.class == Point and point_2.class == Point
+        @point_1 = point_1
+        @point_2 = point_2
+        @length = @point_1.calculate_distance_to(@point_2)
+      else
+        raise("Line should be initialized with 2 point objects")
+      end
+    end
+  end
 end
